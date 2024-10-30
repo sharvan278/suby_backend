be6fb5d04c974f92fbfef9e45639d7e060892597
sharvan278 <naraparajusharvan01@gmail.com>
2024-10-30 12:47:34 +0530

    changes made in index,routes

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const vendorRoutes = require('./routes/vendorRoutes');
 const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
-
+const path = require('path');
 
 const app = express();
-const port = 4000;
+const port = process.env.port || 4000;
 app.use(bodyParser.json()); // Add this line
+app.use('/uploads',express.static('uploads'));
 
 app.use('/home',(req,res)=>{
     res.send('Welcome to home page');
--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -7,4 +7,11 @@ router.post('/add-firm',verifyToken, firmController.addFirm);
 // router.post('/login', vendorController.vendorLogin);
 router.delete('/:fid',firmController.deleteFirm);
 
+router.get('/uploads/:imageName',(req,res)=>{
+    const imageName = req.params.imageName;
+    res.headersSent('Content-Type','image/jpeg');
+    res.sendFile(path.join(__dirname,'../uploads',imageName));
+
+});
+
 module.exports = router;
